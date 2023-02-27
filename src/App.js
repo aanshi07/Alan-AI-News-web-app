a7395c1a32af440e13ad2cc0646cc8f86ca0bfb1
Aanshi <aanshilahoti677@gmail.com>
2023-02-27 17:49:00 +0530

    Completed News-web app using Alan Ai

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React,{ useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import wordsToNumbers from 'words-to-numbers';
+
+import NewsCards  from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
+const alankey = 'f2bc0953f60ce9f5fefa3d70c9100f6c2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+
+   const [newsArticles, setNewsArticles] = useState([]);
+   const [activeArticle, setActiveArticle] = useState(-1);
+   const classes = useStyles();
+    useEffect(() => {
+        alanBtn({
+            key: alankey,
+            onCommand: ({command, articles, number}) => {
+                if(command === 'newHeadlines'){
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if (command === 'highlight'){
+                  setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                } else if(command === 'open'){
+                  const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy : true}) : number;
+                  const article = articles[parsedNumber - 1];
+
+                  if(parsedNumber > 20){
+                    alanBtn().playText('Please try that again');
+                  }else if(article){
+                    window.open(article.url, '_blank');
+                    alanBtn().playText('Opening...');
+                  }
+
+                  window.open(articles[number].url, '_blank');
+                }
+            }
+        })
+    }, [])
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+         <img src= "https://voicebot.ai/wp-content/uploads/2019/10/alan.jpg" className={classes.alanLogo} alt="alan logo"/>
+      </div>
+       <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+    </div>
+  )
+}
+
+export default App;
